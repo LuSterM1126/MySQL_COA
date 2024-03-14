c05c266604acb63504182e12fa6e2a6ee703c3b0
LuSterM <li3854044@gmail.com>
2024-03-14 11:16:50 +0800

    提交2

---
+++ MySQL.session.sql
@@ -0,0 +1,11 @@
+-- Active: 1709821089199@@127.0.0.1@3306@c1
+-- SELECT workid, name FROM emp WHERE workid >= 3;
+-- SELECT * FROM emp WHERE age = 20;
+-- SELECT * FROM emp WHERE idcard IS NOT NULL;
+-- INSERT INTO emp VALUES(7, '7', 'cjw','0', 30, '', '', '2003-01-01');
+-- SELECT * FROM emp WHERE address = '';
+-- SELECT * FROM emp WHERE age BETWEEN 19 AND 20 OR id BETWEEN 1 AND 5;
+-- SELECT * FROM emp WHERE age IN(19, 30);
+-- SELECT * FROM emp WHERE name LIKE '__';
+-- SELECT * FROM emp WHERE age LIKE '%0';
+SELECT MAX(age) FROM emp WHERE age = 20;
